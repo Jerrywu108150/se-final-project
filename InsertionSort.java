a1d7aba01c7d3eb37e55c65ccbadc0b1a39130c1
謝瑞峰 <acs108131@gm.ntcu.edu.tw>
2021-03-15 04:00:25 +0000

    Replace InsertionSort.java

--- InsertionSort.java
+++ InsertionSort.java
@@ -8,15 +8,15 @@ public class InsertionSort {
     public InsertionSort(ArrayList<Integer> dataset) {
         this.dataset = new ArrayList<Integer>(dataset);
     }
-
-    public int countNumber() {
-        return dataset.size();
+    
+    public void display(){
+        for(int i=0;i<countNumber();i++){
+            System.out.print(dataset.get(i));
+            if(i<countNumber()-1)System.out.print(" ");
+        }
+        System.out.println();
     }
-
-    public void addNumber(int newNumber) {
-        dataset.add(newNumber);
-    }
-
+    
     public void reverse() {
         for(int i=countNumber()-1;i>=0;i--) {
             System.out.print(dataset.get(i));
@@ -25,27 +25,71 @@ public class InsertionSort {
         System.out.println();
     }
 
-    public void search() {
-        int max=sc.nextInt();
-        int min=sc.nextInt();
-        for(int i=0;i<countNumber();i++)
-        if(dataset.get(i)<=max&&dataset.get(i)>=min)
-        System.out.print(dataset.get(i)+" ");
-        System.out.println();
+    public int countNumber() {
+        return dataset.size();
     }
 
-    public void edit() {
-        int target=sc.nextInt();
-        int modify=sc.nextInt();
-        boolean empty=true;
-        for(int i=0;i<countNumber();i++) {
-            if(dataset.get(i)==target){
-            dataset.set(i,modify);
-            empty=false;
-            }
+    public void addNumber(int newNumber) {
+        dataset.add(newNumber);
+    }
+    
+    public void random(int amount, int n1, int n2) {
+        int max, min;
+        if(n1>n2){
+            max=n1;
+            min=n2;
+        }else{
+            max=n2;
+            min=n1;
         }
-        if(empty)edit();
+        for(int i=0;i<amount;i++)
+        addNumber((int)(Math.random()*(max-min+1)+min));
+    }
+    
+    public boolean search(int n1, int n2) {
+        boolean notEmpty=false;
+        int max,min;
+        if(n1>n2){
+            max=n1;
+            min=n2;
+        }else{
+            max=n2;
+            min=n1;
+        }
+        for(int i=0;i<countNumber();i++)
+        if(dataset.get(i)<=max&&dataset.get(i)>=min){
+            System.out.print(dataset.get(i)+" ");
+            notEmpty=true;
+        }
+        System.out.println();
+        return notEmpty;
     }
 
-
+    public boolean edit(int target, int modify) {
+        boolean targetExist=false;
+        for(int i=0;i<countNumber();i++)
+        if(dataset.get(i)==target) {
+            dataset.set(i,modify);
+            targetExist=true;
+        }
+        return targetExist;
+    }
+    
+    public boolean delete(int target) {
+        boolean targetExist=false;
+        for(int i=0;i<countNumber();i++)
+        if(dataset.get(i)==target){
+            dataset.remove(i);
+            targetExist=true;
+        }
+        return targetExist;
+    }
+    
+    public double mean() {
+        double sum=0;
+        for(int i=0;i<countNumber();i++)
+        sum+=dataset.get(i);
+        return sum/countNumber();
+    }
+    
 }
