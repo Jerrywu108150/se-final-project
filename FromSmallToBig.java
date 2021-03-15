cfd6b2b17f8704ec5f19d853d317157046aade26
謝瑞峰 <acs108131@gm.ntcu.edu.tw>
2021-03-15 09:21:44 +0000

    Replace FromSmallToBig.java

--- FromSmallToBig.java
+++ FromSmallToBig.java
@@ -7,7 +7,8 @@ public class FromSmallToBig extends InsertionSort {
     }
 
     public void stepByStep() {
-        for (int i = 1; i < countNumber(); i++) {
+        int cn=countNumber();
+        for (int i = 1; i < cn; i++) {
             int n = dataset.get(i);
             int j = i - 1;
             while (j >= 0 && dataset.get(j) > n) {
@@ -16,13 +17,48 @@ public class FromSmallToBig extends InsertionSort {
             }
             dataset.set(j + 1, n);
             System.out.print("Step " + i + ": ");
-            for (int k = 0; k < countNumber(); k++) {
+            for (int k = 0; k < cn; k++) {
                 System.out.print(dataset.get(k));
-                if (k < countNumber() - 1)
+                if (k < cn - 1)
                     System.out.print(" ");
             }
             System.out.println();
         }
+    }
+    
+    public void spinSlash() {
+        int cn=countNumber();
+        for (int i=1;i<cn;i++) {
+            int n = dataset.get(i);
+            int j = i - 1;
+            while (j >= 0 && dataset.get(j) > n) {
+                dataset.set(j + 1, dataset.get(j));
+                j--;
+            }
+            dataset.set(j + 1, n);
+        }
+        for (int k = 0; k < cn; k++) {
+            System.out.print(dataset.get(k));
+            if (k < cn - 1)
+                System.out.print(" ");
+        }
+        System.out.println();
+    }
+    
+    public int timeComplexity() {
+        int tc=0, cn=countNumber();
+        ArrayList<Integer>copy=new ArrayList<Integer>(dataset);
+        for (int i = 1; i < cn; i++) {
+            int n = copy.get(i);
+            int j = i - 1;
+            while (j >= 0 && copy.get(j) > n) {
+                copy.set(j + 1, copy.get(j));
+                j--;
+                tc++;
+            }
+            copy.set(j + 1, n);
+        }
+        return tc;
     }
 
 }
