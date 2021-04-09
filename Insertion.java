fde748d86269fe6d616085a23c6b193f16b7384f
謝瑞峰 <acs108131@gm.ntcu.edu.tw>
2021-04-09 16:41:07 +0000

    Upload New File

---
+++ Insertion.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Insertion extends Sort {
+    public Insertion() {
+        super();
+    }
+
+    public Insertion(ArrayList<String> input) {
+        super(input);
+    }
+
+    public Insertion(String... input) {
+        super(input);
+    }
+}
