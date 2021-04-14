c2b5c4de7a4730791fee861b5de572e2fcc8821f
竣宇 吳 <acs108150@gm.ntcu.edu.tw>
2021-04-14 02:12:51 +0000

    Update Sort.java

--- Sort.java
+++ Sort.java
@@ -80,7 +80,7 @@ public class Sort {
         }
         String temp = "Before sort:";
         for (String str : dataset) {
-            temp += "\t" + str;
+            temp += "\t\t" + str;
         }
         record.add(temp);
         isNumber = judgeType();
